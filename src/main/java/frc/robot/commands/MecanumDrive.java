90d833dfee870130283c535ae45ccc406e352a2d
momo-beep <ilovedrawinga@gmail.com>
2023-02-03 15:17:49 -0500

    Mecanum Drive refactor
    Changed drive train to be better and mecanum drive command.

--- src/main/java/frc/robot/commands/MecanumDrive.java
+++ src/main/java/frc/robot/commands/MecanumDrive.java
@@ -27,36 +27,28 @@ public class MecanumDrive extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double horizontal = horizontalFunction.getAsDouble() / 2;
-    double vertical = verticalFunction.getAsDouble() / 2;
-    double pivot = pivotFunction.getAsDouble() / 2;
+    double horizontal = horizontalFunction.getAsDouble();
+    double vertical = verticalFunction.getAsDouble();
+    double pivot = pivotFunction.getAsDouble();
 
-    if(Math.abs(horizontal) >= Constants.DEADZONE || Math.abs(vertical) >= Constants.DEADZONE || Math.abs(pivot) >= Constants.DEADZONE){
-      drivetrain.setFrontLeftMotor(-pivot + (vertical - horizontal));
-      drivetrain.setBackLeftMotor(-pivot + (vertical + horizontal));
-      drivetrain.setFrontRightMotor(pivot + (vertical + horizontal));
-      drivetrain.setBackRightMotor(pivot + (vertical - horizontal));
-    }
-    else{
-      drivetrain.setFrontLeftMotor(0);
-      drivetrain.setBackLeftMotor(0);
-      drivetrain.setFrontRightMotor(0);
-      drivetrain.setBackRightMotor(0);
+    if (Math.abs(horizontal) >= Constants.DEADZONE || Math.abs(vertical) >= Constants.DEADZONE
+        || Math.abs(pivot) >= Constants.DEADZONE) {
+      drivetrain.driveCartesian​(horizontal, vertical, pivot);
+    } else {
+      drivetrain.driveCartesian​(0, 0, 0);
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    drivetrain.setFrontLeftMotor(0);
-    drivetrain.setBackLeftMotor(0);
-    drivetrain.setFrontRightMotor(0);
-    drivetrain.setBackRightMotor(0);
+    drivetrain.driveCartesian​(0, 0, 0);
   }
 
   // Returns true when the command should end.
