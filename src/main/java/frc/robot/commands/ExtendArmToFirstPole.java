c71562d59a1f67c188ae80d60281c51a207b0ae0
Zachary <ztcollazo08@gmail.com>
2023-02-23 13:15:35 -0500

    Add placeholder for arm location commands

---
+++ src/main/java/frc/robot/commands/ExtendArmToFirstPole.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Arm.A_extendTo;
+import frc.robot.commands.Arm.A_pivotToSLOW;
+import frc.robot.subsystems.Arm;
+
+public class ExtendArmToFirstPole extends SequentialCommandGroup {
+  public ExtendArmToFirstPole(Arm arm) {
+    addCommands(new A_pivotToSLOW(0, arm), new A_extendTo(0, arm));  // Replace with angle and length of pole
+  }
+}
