1aa3a08b89cb6dd0a7449958b6bfb5fac99c53f6
ChickenWingJohnny <sch.7hs@gmail.com>
2023-03-03 08:10:59 -0500

    merging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -53,7 +53,7 @@ public class Robot extends TimedRobot {
   /**  This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();s
+    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
     // schedule the autonomous command (example)
     if (m_AutonomousCommandSimple != null && imu.isIMUReady()) {
       m_AutonomousCommandSimple.schedule();
