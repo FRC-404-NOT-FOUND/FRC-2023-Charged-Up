eca7892fbb322c5944989318ef59ee13b8bdce12
Zachary <ztcollazo08@gmail.com>
2023-03-15 10:01:36 -0400

    Cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.ArmRaiseContinuous;
 import frc.robot.commands.MecanumDrive;
 import frc.robot.commands.autonomous.AutonomousCommandSimple;
-import frc.robot.commands.autonomous.MoveToAprilTag;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Grabber;
@@ -71,9 +68,6 @@ public class RobotContainer {
   private final Trigger oi_gToggleCone = new Trigger(() -> OI.gamepad.getAButton());
   private final Trigger oi_gToggleCube = new Trigger(() -> OI.gamepad.getBButton());
 
-  private final Trigger oi_moveToAprilTag = new Trigger(() -> OI.gamepad.getRightStickButton());
-  private boolean shouldStart = true;
-
  // private final Trigger oi_iArduinoReconnect = new Trigger(() -> OI.gamepad.getBackButtonPressed());
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -107,9 +101,6 @@ public class RobotContainer {
 
     //Toggles Fine Control
     oi_driveFine.toggleOnTrue(mecanumDriveFine);
-
-    var aprilTag = new MoveToAprilTag(s_drivetrain);
-    oi_moveToAprilTag.toggleOnTrue(new ConditionalCommand(aprilTag, Commands.run(() -> aprilTag.cancel()), () -> shouldStart).finallyDo((interruped) -> shouldStart = !shouldStart));
 
     oi_defaultPosition.onTrue(s_arm.moveToDefault());
     oi_cone1Place.onTrue(s_arm.moveArmTo(Constants.FIRST_CONE_ANGLE, Constants.FIRST_CONE_EXTENSION).withInterruptBehavior(InterruptionBehavior.kCancelSelf));
--- src/main/java/frc/robot/subsystems/Grabber.java
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -36,9 +36,7 @@ public class Grabber extends SubsystemBase {
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+  public void periodic() {}
 
   public G_Hopper getHopper(){
     return hopper;
